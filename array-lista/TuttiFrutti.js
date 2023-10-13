83c7b41ddff705dc456f79f7c04a11d653618e3a
davidelunetta <davidelunetta.27@gmail.com>
2023-10-13 16:14:09 +0200

    added fruit to array

--- array-lista/TuttiFrutti.js
+++ array-lista/TuttiFrutti.js
@@ -1,2 +1,5 @@
 const fruttaFrigorifero = 
 ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+const pesca = 'pesca';
+fruttaFrigorifero.push(pesca);
+console.log(fruttaFrigorifero)
