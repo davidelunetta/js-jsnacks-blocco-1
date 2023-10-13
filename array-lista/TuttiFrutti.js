1940d3cfc12ae117079b8a4629e6084b6b7c1bd8
davidelunetta <davidelunetta.27@gmail.com>
2023-10-13 16:44:57 +0200

    added for cicle

--- array-lista/TuttiFrutti.js
+++ array-lista/TuttiFrutti.js
@@ -2,4 +2,16 @@ const fruttaFrigorifero =
 ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 const pesca = 'pesca';
 fruttaFrigorifero.push(pesca);
-console.log(fruttaFrigorifero)
+console.log(fruttaFrigorifero);
+let cocomeroPresente = false;
+for (let i = 0; i < fruttaFrigorifero.length; i++) {
+    if (fruttaFrigorifero[i] === 'cocomero') {
+        cocomeroPresente = true;
+        break;
+    }
+}
+if (cocomeroPresente) {
+    console.log("Trovato! Devo solo preparare il cocktail.");
+} else {
+    console.log("Oh no, devo uscire a comprare il cocomero!");
+}
